bc79e665dc867a3c79b81a6050f720f7a83f6a81
siri.surab <siri.surab@gmail.com>
2020-05-14 01:33:21 -0500

    python 2

---
+++ 03-Python/2/Activities/02-Ins_SimpleLoops/Unsolved/simple_loops.py
@@ -0,0 +1,12 @@
+# loop through a range of numbers (from 0 upto a provided number)
+
+
+# loop through a range of numbers (from first number up until it reaches the second number)
+
+
+# loop through the list below
+words = ["Peanut", "Butter", "Jelly", "Time", "Is", "Now"]
+
+# loop to print out "Whee! Merry-Go-Rounds are great!"
+# ask user "Would you like to go on the Merry-Go-Round again? "
+# keep looping until the user says No
